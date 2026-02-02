0f0aa0d36bc31bb19eabc9597221a77e18716e58
priz80 <priz8090@gmail.com>
2026-02-02 23:16:22 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,15 +3,13 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 import Burger from './components/Burger';
-import Faq from './components/Faq';
-import Reviews from './components/Reviews';
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
     <Burger />
-    <Faq />
-    <Reviews />
   </React.StrictMode>
 );
